71521dda75b8e8fb7691b38d41b60bca3edc4911
Lehoangminh-dab <hoangminh7c12@gmail.com>
2024-11-23 10:40:06 +0700

    feat(uploader.js): tuned batch flunctuations.

--- CS2-no-message-queue-testing/src/producer/image-uploader/uploader.js
+++ CS2-no-message-queue-testing/src/producer/image-uploader/uploader.js
@@ -10,7 +10,7 @@ const UPLOAD_LIMIT_WINDOW_MS = 60 * 1000; // 1 minute
 const UPLOAD_LIMIT_MAX_REQUESTS = 100;
 const UPLOAD_LIMIT_MESSAGE = "Too many uploads from this IP, please try again later";
 
-const BATCH_FLUNCTUATIONS = [5, 1, 2];
+const BATCH_FLUNCTUATIONS = [5, 1, 2, 10, 20];
 const MAX_FILES = 1000;
 const DELAY_BETWEEN_BATCHES = 5000; // milliseconds
 
